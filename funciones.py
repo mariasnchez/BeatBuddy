9231a7c7b7591a7ebf04fcfdd13244b4bd9ecd84
PabloMartinTrujillo <pablo.martin.alu@iescampanillas.com>
2024-03-04 20:44:21 +0100

    asd

--- funciones.py
+++ funciones.py
@@ -18,10 +18,10 @@ def fuentes():
 def header():
     return """
         <div id="header" class="d-flex justify-content-end align-items-center pe-2 pt-2">
-            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Asistente_Musical" target="_self">Asistente Musical</a></div>
-            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Reconocimiento_Facial" target="_self">Reconocimiento facial</a></div>
-            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Chatbot" target="_self">Chatbot</a></div>
-            <div class="link-div fs-4" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Sobre_Nosotros" target="_self">Sobre nosotros</a></div>
+            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Asistente_Musical" target="_self">Asistente Musical</a></div>
+            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Reconocimiento_Facial" target="_self">Reconocimiento facial</a></div>
+            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Chatbot" target="_self">Chatbot</a></div>
+            <div class="link-div fs-4" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Sobre_Nosotros" target="_self">Sobre nosotros</a></div>
         </div>
     """
 
@@ -29,12 +29,12 @@ def header2():
     return """
         <div id="header" class="d-flex justify-content-end align-items-center pe-2 pt-2">
             <div class="logo">
-                <a href="https://beatbuddyapp.streamlit.app" target="_self"><img src="https://beatbuddyapp.streamlit.app:443/~/+/media/cdf0488c738115d7f79bedc0dbba33fcddaf432f3e57a3d5eea75553.png" alt="Logo"></a>
+                <a href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app" target="_self"><img src="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app:443/~/+/media/cdf0488c738115d7f79bedc0dbba33fcddaf432f3e57a3d5eea75553.png" alt="Logo"></a>
             </div>
-            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Asistente_Musical" target="_self">Asistente Musical</a></div>
-            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Reconocimiento_Facial" target="_self">Reconocimiento facial</a></div>
-            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Chatbot" target="_self">Chatbot</a></div>
-            <div class="link-div fs-4" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://beatbuddyapp.streamlit.app/Sobre_Nosotros" target="_self">Sobre nosotros</a></div>
+            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Asistente_Musical" target="_self">Asistente Musical</a></div>
+            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Reconocimiento_Facial" target="_self">Reconocimiento facial</a></div>
+            <div class="link-div fs-4 me-3" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Chatbot" target="_self">Chatbot</a></div>
+            <div class="link-div fs-4" style="font-family: 'DM Sans', 'Source Sans Pro'", sans-serif;"><a class="header-link" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Sobre_Nosotros" target="_self">Sobre nosotros</a></div>
         </div>
     """
 
@@ -166,7 +166,7 @@ def home():
         </head>
         <div id="inicio-body">
             <div id="img-container">
-                <img id="inicio-img" src="https://beatbuddyapp.streamlit.app:443/~/+/media/cdf0488c738115d7f79bedc0dbba33fcddaf432f3e57a3d5eea75553.png" alt="BeatBuddy"/>
+                <img id="inicio-img" src="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app:443/~/+/media/cdf0488c738115d7f79bedc0dbba33fcddaf432f3e57a3d5eea75553.png" alt="BeatBuddy"/>
             </div>
             <div id="inicio-beatbuddy">
                 <div id="inicio-titulo" class="francois-one-regular ml6">
@@ -175,7 +175,7 @@ def home():
                     </span>
                 </div>
                 <div id="inicio-descrip" class="dm-sans-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper, felis et elementum efficitur, velit ipsum congue tellus, sit amet placerat massa tellus at sem. </div>
-                <a id="btn-empezar" class="dm-sans-4" href="https://beatbuddyapp.streamlit.app/Asistente_Musical">¡Empieza aquí!</a>
+                <a id="btn-empezar" class="dm-sans-4" href="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app/Asistente_Musical">¡Empieza aquí!</a>
             </div>
         </div>
         <script>{animejs}</script>
@@ -259,7 +259,7 @@ def sobre_nosotros():
             <div class="w-25 h-50 d-flex flex-column justify-content-around align-items-center">
                 <div id="linkedin-div" class="d-flex flex-column justify-content-start">
                     <div class="d-flex align-items-center">
-                        <img id="linkedin-img" src="https://beatbuddyapp.streamlit.app:443/~/+/media/ff9bf60bd47e2d8590057ce1913f1eb466c7a4b458d2d78fd7a18c8c.png" alt="Linkedin"/>
+                        <img id="linkedin-img" src="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app:443/~/+/media/ff9bf60bd47e2d8590057ce1913f1eb466c7a4b458d2d78fd7a18c8c.png" alt="Linkedin"/>
                         <span class="text-white fs-4 ps-1">Linkedin</span>
                     </div>
                     <a class="about-us-links fs-5 ps-1 pt-1" href="https://www.linkedin.com/in/itsupportspecialist-sysadmin-christianmd/">Christian Martín Díaz</a>
@@ -268,7 +268,7 @@ def sobre_nosotros():
                 </div>
                 <div id="github-div" class="d-flex flex-column justify-content-start">
                     <div class="d-flex align-items-center">
-                        <img id="github-img" src="https://beatbuddyapp.streamlit.app:443/~/+/media/d96c7815c16b6bb69483387073e3a78b19728b28d57e1d916101759e.png" alt="Github"/>
+                        <img id="github-img" src="https://prueba-app-2-alsnfdpxrmwiwyucrmdf8r.streamlit.app:443/~/+/media/d96c7815c16b6bb69483387073e3a78b19728b28d57e1d916101759e.png" alt="Github"/>
                         <span class="text-white fs-4 ps-1">Github</span>
                     </div>
                     <a class="about-us-links fs-5 ps-1 pt-1" href="https://github.com/chrismartindiaz">Christian Martín Díaz</a>
